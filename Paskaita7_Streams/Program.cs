2a30ddebd502ae60e825bee5d2098ef3f0593730
Gaudrius <gaudriusk@gmail.com>
2024-07-10 22:14:49 +0300

    Added Paskaita7_Streams

---
+++ Paskaita7_Streams/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Paskaita7_Streams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var smalsumas = new Smalsumas();
+            smalsumas.OptimizacijosUzdavinys("data.txt", 5_000);
+            Console.WriteLine("---------------Big Data---------------");
+            // destytojas turejo 700_000 eiluciu teskta i bigData.txt isidejes 
+            smalsumas.OptimizacijosUzdavinys("bigData.txt", 5_000);
+        }
+    }
+}
---
+++ Paskaita7_Streams/Smalsumas.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+namespace Paskaita7_Streams
+{
+    internal class Smalsumas
+    {
+        public void OptimizacijosUzdavinys(string path, int samples)
+        {
+            var timer = new Stopwatch();
+            Console.WriteLine("Tikriname File.ReadAllLines() efektyvuma");
+            timer.Start();
+
+            for (int i = 0; i < samples; i++)
+            {
+                var lines = File.ReadAllLines(path);
+                foreach (var line in lines)
+                {
+                    // prasukam cikla, kad tikrai butu failo ...(19.13)
+                }
+            }
+            timer.Stop();
+            var readAllLinesTime = timer.ElapsedMilliseconds;
+            Console.WriteLine($"File.ReadAllLines() uztruko: {readAllLinesTime}ms");
+
+            Console.WriteLine("Tikriname File.ReadLines() efektyvuma");
+            timer.Restart();
+
+            for (int i = 0; i < samples; i++)
+            {
+                var lines = File.ReadLines(path);
+                foreach (var line in lines)
+                {
+                    // prasukam cikla, kad tikrai butu failo ...(19.13)
+                }
+            }
+            timer.Stop();
+            var readLinesTime = timer.ElapsedMilliseconds;
+            Console.WriteLine($"File.ReadLines() uztruko: {readLinesTime}ms");
+
+            Console.WriteLine("Tikriname File.ReadLines() kartu su ToArray() efektyvuma");
+            timer.Restart();
+
+            for (int i = 0; i < samples; i++)
+            {
+                var lines = File.ReadLines(path).ToArray();
+                foreach (var line in lines)
+                {
+                    // prasukam cikla, kad tikrai butu failo ...(19.13)
+                }
+            }
+            timer.Stop();
+            var readLinesToArrayTime = timer.ElapsedMilliseconds;
+            Console.WriteLine($"File.ReadLines() kartu su ToArray() uztruko: {readLinesToArrayTime}ms");
+
+            Console.WriteLine("Tikriname File.ReadLines(), o poto Split() efektyvuma");
+            timer.Restart();
+
+            for (int i = 0; i < samples; i++)
+            {
+                var lines = File.ReadAllText(path).Split('\n');
+                foreach (var line in lines)
+                {
+                    // prasukam cikla, kad tikrai butu failo ...(19.13)
+                }
+            }
+            timer.Stop();
+            var readAllTextTime = timer.ElapsedMilliseconds;
+            Console.WriteLine($"File.ReadAllText(), o poto Split() uztruko: {readAllTextTime}ms");
+        }
+    }
+}
